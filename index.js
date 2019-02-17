d14d4e43b6b637b72a299a476af8ea3ec0a42d1b
CJ Horlebein <chrishorlebein@gmail.com>
2019-02-16 23:34:30 -0500

    completed exercise

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Write your app here! (HINT: First thing should be a call to ReactDOM.render()... )
+
+const { Component } = React;
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {colors: [
+                { color: '#FF0000', locked: false },
+                { color: '#00FF00', locked: false },
+                { color: '#0000FF', locked: false },
+                { color: '#00FFFF', locked: false },
+                { color: '#FF00FF', locked: false },
+                { color: '#FFFF00', locked: false }
+            ]}
+        }
+    setColors(colors){
+        this.setState({colors});
+    }
+
+    lockColor(num){
+        let colors = [...this.state.colors]
+        colors[num].locked = !colors[num].locked
+        this.setState(colors);
+    }
+
+    randomColors(){
+        let newColors = this.state.colors.map(color => {
+            let randColor = ['#'];
+            if(color.locked){return color}
+            for(let i = 0; i < 6; i++){
+                randColor.push(Math.floor(Math.random() * 16).toString(16).toUpperCase());
+            }
+            return {color: randColor.join(''), locked: color.locked};
+        })
+        this.setColors(newColors);
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="text-center bg-dark fixed-top">
+                    <button onClick={() => this.randomColors()} className="btn btn-secondary m-1">RANDOMIZE COLORS</button>
+                </div>
+                <div className="w-100 d-flex" style={{height: "100vh"}}>
+                    {this.state.colors.map((color, i) => (
+                            <div key={i} style={{backgroundColor: color.color}} className="w-100 d-flex flex-column align-items-center justify-content-center">
+                                <h1>{color.color}</h1>
+                                <button onClick={() => this.lockColor(i)} className="btn btn-dark">{color.locked ? 'locked' : 'unlocked'}</button>
+                            </div>
+                        )
+                    )}
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App  />, document.getElementById('root'));
+
+
+
+
